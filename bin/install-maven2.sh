910e1a05b886a8def3b5ab6ecbf2605c7cf68526
Terje Dahl <terje@andante.no>
2017-02-16 14:03:09 +0100

    Now with checksums.

--- bin/install-maven2.sh
+++ bin/install-maven2.sh
@@ -11,6 +11,7 @@ mvn install:install-file \
  -DgroupId=$GROUP -DartifactId=$ARTIFACT -Dversion=$VERSION \
  -DupdateReleaseInfo=true \
  -Dpackaging=jar \
+ -DcreateChecksum=true \
  -DpomFile=target/$ARTIFACT-$VERSION.pom \
  -Dfile=target/$ARTIFACT-$VERSION.jar \
  -Djavadoc=target/$ARTIFACT-$VERSION-javadoc.jar \
